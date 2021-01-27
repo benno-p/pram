9ed6df7959f49ca1bb6eccdeb1ce6e4f0736a09f
Benno <40667946+benno-p@users.noreply.github.com>
2021-01-27 18:31:28 +0100

    updated

--- php/ajax/odk/odk_forms.js.php
+++ php/ajax/odk/odk_forms.js.php
@@ -4,53 +4,53 @@ include "../../properties.php";
 
 
 //CHECK IF PHOTOS ARE WRITTEN ON SERVER IF NOT WRITE
-//$dbconn = pg_connect("hostaddr=$ODK_DBHOST port=$ODK_PORT dbname=$ODK_DBNAME user=$ODK_LOGIN password=$ODK_PASS")
-//or die ('Connexion impossible :'. pg_last_error());
-//$result = pg_prepare($dbconn, "sql", "
-//with data_ as (
-//    SELECT 
-//    odk.uuid_core, 
-//    odk.id_user, 
-//    odk.date_import, 
-//    odk.date_form, 
-//    odk.status
-//    FROM $odk_import_odk_pram odk
-//    WHERE 
-//    odk.status = $1 AND
-//    odk.id_user = $2
-//)
-//SELECT 
-//    obs.\"_URI\",
-//    data_.uuid_core,
-//    encode(photo.\"VALUE\", 'base64') as img, 
-//    replace(trim(photo.\"_URI\", 'uuid:'), '-','')||'.jpg' as id_
-//    FROM 
-//    data_ LEFT JOIN 
-//    $odk_pram_obs obs ON (((obs.\"_URI\")::text = (data_.uuid_core)::text))
-//    LEFT JOIN $odk_pram_obs_ajout_photo ap ON (((ap.\"_PARENT_AURI\")::text = (obs.\"_URI\")::text))
-//    LEFT JOIN $odk_pram_obs_photo_mare_bn blb_bn ON (((ap.\"_URI\")::text = (blb_bn.\"_PARENT_AURI\")::text))
-//    LEFT JOIN $odk_pram_obs_photo_mare_ref blb_ref ON (((blb_bn.\"_URI\")::text = (blb_ref.\"_DOM_AURI\")::text))
-//    LEFT JOIN $odk_pram_obs_photo_mare_blb photo ON (((blb_ref.\"_SUB_AURI\")::text = (photo.\"_URI\")::text))
-//WHERE photo.\"VALUE\" IS NOT NULL;
-//");
-//// --photo.\"_URI\",
-//// --photo.\"VALUE\"
-//
-//$result = pg_execute($dbconn, "sql",array(
-//'pending', $_SESSION['email']
-//));
-//
-//while($row = pg_fetch_row($result))
-//{
-//    $image = pg_unescape_bytea($row[2]);
-//    $link_to_write = $odk_photos_directory.$row[3];
-//    if (file_exists($link_to_write)) {
-//        //do nothing
-//    } else {
-//        file_put_contents($link_to_write, base64_decode($image) );
-//    }
-//};
-//pg_close($dbconn);
+$dbconn = pg_connect("hostaddr=$ODK_DBHOST port=$ODK_PORT dbname=$ODK_DBNAME user=$ODK_LOGIN password=$ODK_PASS")
+or die ('Connexion impossible :'. pg_last_error());
+$result = pg_prepare($dbconn, "sql", "
+with data_ as (
+    SELECT 
+    odk.uuid_core, 
+    odk.id_user, 
+    odk.date_import, 
+    odk.date_form, 
+    odk.status
+    FROM $odk_import_odk_pram odk
+    WHERE 
+    odk.status = $1 AND
+    odk.id_user = $2
+)
+SELECT 
+    obs.\"_URI\",
+    data_.uuid_core,
+    encode(photo.\"VALUE\", 'base64') as img, 
+    replace(trim(photo.\"_URI\", 'uuid:'), '-','')||'.jpg' as id_
+    FROM 
+    data_ LEFT JOIN 
+    $odk_pram_obs obs ON (((obs.\"_URI\")::text = (data_.uuid_core)::text))
+    LEFT JOIN $odk_pram_obs_ajout_photo ap ON (((ap.\"_PARENT_AURI\")::text = (obs.\"_URI\")::text))
+    LEFT JOIN $odk_pram_obs_photo_mare_bn blb_bn ON (((ap.\"_URI\")::text = (blb_bn.\"_PARENT_AURI\")::text))
+    LEFT JOIN $odk_pram_obs_photo_mare_ref blb_ref ON (((blb_bn.\"_URI\")::text = (blb_ref.\"_DOM_AURI\")::text))
+    LEFT JOIN $odk_pram_obs_photo_mare_blb photo ON (((blb_ref.\"_SUB_AURI\")::text = (photo.\"_URI\")::text))
+WHERE photo.\"VALUE\" IS NOT NULL;
+");
+// --photo.\"_URI\",
+// --photo.\"VALUE\"
+
+$result = pg_execute($dbconn, "sql",array(
+'pending', $_SESSION['email']
+));
+
+while($row = pg_fetch_row($result))
+{
+    $image = pg_unescape_bytea($row[2]);
+    $link_to_write = $odk_photos_directory.$row[3];
+    if (file_exists($link_to_write)) {
+        //do nothing
+    } else {
+        file_put_contents($link_to_write, base64_decode($image) );
+    }
+};
+pg_close($dbconn);
 
 
 
