5d8ebfe6bed47adf4bb67b2922b74b6f4b15ea25
Benno <b.perceval@superceval.fr>
2021-10-06 18:24:48 +0200

    up
    a

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <title>PRAM Normandie</title>
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap-5.0.0/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="css/pram.css" rel="stylesheet">
     <!-- Custom Fonts -->
@@ -45,24 +45,23 @@
             <span style="color:#fff;opacity:0.8;font-size:24px;">&nbsp;</span>
         </div>
         <!-- /.row -->
-        <div class="row">
-            <div class="col-lg-6 mt-2 ml-2">
+        <div class="">
+            <div class="col-lg-6 m-2">
                 <h4>Connexion à l'application PRAM Normandie</h4>
                 <form role="form">
-                    <div class="form-group">
-                        <label for="courriel">Identifiant :</label>
-                        <input id="courriel" class="form-control" placeholder="courriel@mail.com" onblur="" >
+                    <div class="input-group w-50">
+                        <span class="input-group-text justify-content-center col-2" id="user"><i class="fas fa-user"></i></span>
+                        <input id="courriel" type="text" class="form-control col-10 " placeholder="courriel@mail.com" aria-label="courriel@mail.com" aria-describedby="user" >
                     </div>
-                    <div class="form-group">
-                        <label for="pwd">Mot de passe :</label>
-                        <input id="pwd" type="password" class="form-control" placeholder="xxxxxx" onblur="" >
+                    <div class="input-group w-50 my-2">
+                        <span class="input-group-text justify-content-center col-2" id="passwordLabel"><i class="fas fa-key"></i></span>
+                        <input id="pwd" type="password" class="form-control col-10 " placeholder="MoTDePassE" aria-label="MoTDePassE" aria-describedby="passwordLabel" >
                     </div>
-                    <div class="form-group">
-                        <!--<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Valider" alt="Submit">-->
-                        <button type="button" id="signin" class="btn btn-lg btn-primary">Valider</button>
+                    <div class="my-2">
+                        <button type="button" id="signin" class="btn btn-primary">Valider</button>
                     </div>
                 </form>
-                <a class="create_account point_er" data-toggle="modal" data-target="#ModalLogin">Création de compte</a>
+                <a class="create_account point_er" data-toggle="modal" data-target="#CreateAccount">Création de compte</a>
             </div>
             <div class="col-lg-6">
             </div>
@@ -76,83 +75,81 @@
     <!-- /#wrapper -->
 
 
-
-
-<div id="ModalLogin" class="modal fade" >
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header d-flex flex-column">
-                <div class="d-flex flex-inline w-100 mb-1">
-                    <div class="mr-auto"><h3 class="modal-title" id="">Bienvenu !</h3></div>
-                    <div class=""><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button></div>
-                </div>
-                <div class="d-flex flex-column">
-                    <div class="alert alert-info">
-                    <p class="small">Pour vous inscrire, veuillez renseigner les 3 champs suivants et accepter les conditions générales d'utilisation.</p>
-                    <p class="small">Une fois le compte activé, vous pourrez contribuer au recensement des mares et à la protection de la biodiversité ! (Merci d'avance)</p>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="modal-body mx-2" >
-                <div class="d-flex w-100 flex-column">
-                    <div class="d-flex">
-                        <div class="mr-auto flex-column">
-                            <div class="form-group flex-column">
-                                <label for="inscription_mail">Courriel</label>
-                                <input type="email" class="form-control" id="inscription_mail" aria-describedby="emailHelp" >
-                                <small id="emailHelp" class="form-text text-muted">Votre e-mail servira d'identifiant.</small>
-                            </div>
-                            <div class="form-group flex-column">
-                                <label for="inscription_pwd">Mot de passe</label>
-                                <input type="password" class="form-control" id="inscription_pwd" placeholder="zZZ11zz&&" aria-describedby="pwdHelp" minlength="8" required >
-                                <small id="pwdHelp" class="form-text text-muted">8 caractères dont 1 Majuscule, 1 chiffre, 1 caractère spécial</small>
-                            </div>
-                            
-                        </div>
-                        
-                        <div class="form-group flex-column">
-                                    <div class="justify-content-center"><label>Je ne suis pas un robot :</label></div>
-                                    <div class="justify-content-center">
-                                        <div class="mb-1" data-toggle="" data-placement="" title="uniquement : ABCDEFGHIJKLMNOPQRSTUVWXYZ" >
-                                            <img class="shadow" src="php/captcha.php"></img>
-                                        </div>
-                                        <div class=""><input id="verif" type="text" class="form-control" placeholder="xxxxxx" onblur="" ></div>
-                                    </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="small">
-                                 <h6 class="font-weight-bold mb-2 mt-2">Conditions d'utilisation :</h6>
-                                En naviguant sur notre site, l’internaute reconnaît avoir pris connaissance et accepté nos conditions générales d’utilisation
-                                 <h6 class="font-weight-bold mb-2  mt-2">Données personnelles :</h6>
-                                Les données personnelles qui peuvent vous être demandées (exclusivement en vue de renseigner/décrire des mares) sont vos nom prénom et adresse mails.
-                                Le traitement de ces données dont le responsable est Mr Benoit Perceval est en conformité avec le Règlement général de protection des données personnelles entré en vigueur le 25 Mai 2018.
-                                Vous pouvez faire valoir vos droits de consultation, de rectification et de suppression en le contactant à l’adresse suivante b.perceval@cen-normandie.fr
-                                Les documents (photos de mares) que vous importerez sur le site ainsi que les caractérisations des mares seront consultables par l'ensemble des utilisateurs.
-                                Le recensement de mares (renseignement d'une fiche, photos) s'il est effectué sur un terrain privé doit se faire en accord avec le propriétaire.
-                                <strong>Le CEN Normandie s'engage à ne jamais revendre ni donner ces informations à des tiers.</strong>
-                                 <h6 class="font-weight-bold mb-2  mt-2">Propriété intellectuelle :</h6>
-                                Le site web a été réalisé par nos géomaticien : Charles Bouteiller et Benoit Perceval.
-                                Il est la propriété de l’association et ne peut faire l’objet de reproduction.
-                                 <h6 class="font-weight-bold mb-2 mt-2">Photographies et contenu :</h6>
-                                Les photographies, vidéos, textes et  illustrations publiés sur le site sont propriété de l’association ou ont fait l’objet de cession de droits.
-                                Ils  ne peuvent faire l’objet d’aucune réutilisation.
-                                 <h6 class="font-weight-bold mb-2 mt-2">Liens hypertextes :</h6>
-                                Le site pramnormandie.com peut contenir des liens hypertextes renvoyant à des pages ou des sites dont le contenu ne peut engager en rien l’association.
-                                Les liens hypertextes renvoyant vers notre site sont les bienvenus lorsqu’ils émanent de sites respectant la législation en vigueur.
-                                <div class="mt-2 font-weight-bold"><input id="cgu_c" value="" type="checkbox" > J'accepte les conditions générales d'utilisation</input></div>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" id="save_create_account" class="btn btn-primary"  aria-hidden="true">Enregistrer</button> <!--data-dismiss="modal"-->
-            </div>
-        </div>
+<!-- Modal -->
+<div class="modal fade" id="CreateAccount" tabindex="-1" aria-labelledby="CreateAccountLabel" aria-hidden="true" >
+  <div class="modal-dialog modal-lg" >
+    <div class="modal-content" style="max-height:90vh;overflow-y:scroll;">
+      <div class="d-flex modal-header">
+		<div class="d-flex w-100">
+			<h4 class="modal-title" id="CreateAccountLabel">Bienvenu !</h4>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+      </div>
+      <div class="modal-body">
+		<div class="alert alert-info m-y-2 small">
+			Pour vous inscrire, veuillez renseigner les champs suivants et lire <strong>attentivement</strong> les conditions d'utilisation.
+		</div>
+		<form class="eventInsFormModal" action="#" method="post">
+			<div class="input-group mt-4">
+				<div class="input-group-prepend">
+				<span class="input-group-text">Courriel et Mot de passe</span>
+				</div>
+				<input id="inscription_mail" type="text" class="form-control" placeholder="courriel">
+				<input id="inscription_pwd"  type="text" class="form-control" placeholder="mot de passe">
+			</div>
+			<small  class="text-muted small mt-1 mb-4">8 caractères dont 1 Majuscule, 1 chiffre, 1 caractère spécial (?!#_)</small>
+			<div class="input-group mt-4">
+				<div class="input-group-prepend">
+				<span class="input-group-text">Je ne suis pas un robot :</span>
+				</div>
+				<input id="verif" type="text" class="form-control" placeholder="MAJUSCULES" onblur="" >
+				<img id="cap" src="php/captcha.php" style="border:solid 1px #ced4da;"></img>
+				<a id="refresh" class="btn btn-default bg-light" style="border:solid 1px #ced4da;"><i  class="fas fa-sync"></i></a>
+			</div>
+			<small  class="text-muted small mt-1 mb-4">Recopiez les caractères ci-dessus</small>
+			<div id="cgu_content" class="my-4">
+				<h3><strong>Conditions Générales d'Utilisation :</strong></h3>
+				<p class="small">
+                    En naviguant sur notre site, l’internaute reconnaît avoir pris connaissance et accepté nos conditions générales d’utilisation
+                </p>
+                <p class="fs-5"><strong>Données personnelles :</strong></p>
+                <p class="small">
+                    Les données personnelles qui peuvent vous être demandées (exclusivement en vue de renseigner/décrire des mares) sont vos nom prénom et adresse mails.
+                    Le traitement de ces données dont le responsable est Mr Benoit Perceval est en conformité avec le Règlement général de protection des données personnelles entré en vigueur le 25 Mai 2018.
+                    Vous pouvez faire valoir vos droits de consultation, de rectification et de suppression en le contactant à l’adresse suivante b.perceval@cen-normandie.fr
+                    Les documents (photos de mares) que vous importerez sur le site ainsi que les caractérisations des mares seront consultables par l'ensemble des utilisateurs.
+                    Le recensement de mares (renseignement d'une fiche, photos) s'il est effectué sur un terrain privé doit se faire en accord avec le propriétaire.
+                </p>
+                <p class="small"><strong>Le CEN Normandie s'engage à ne jamais revendre ni donner ces informations à des tiers.</strong></p>
+                <p class="fs-5"><strong>Propriété intellectuelle :</strong></p>
+                <p class="small">
+                Le site web a été réalisé par nos géomaticien : Charles Bouteiller et Benoit Perceval.
+                Il est la propriété de l’association et ne peut faire l’objet de reproduction.
+                </p>
+                <p class="fs-5"><strong>Photographies et contenu :</strong></p>
+                <p class="small">
+                Les photographies, vidéos, textes et  illustrations publiés sur le site sont propriété de l’association ou ont fait l’objet de cession de droits.
+                Ils  ne peuvent faire l’objet d’aucune réutilisation.
+                </p>
+                <p class="fs-5"><strong>Liens hypertextes :</strong></p>
+                <p class="small">
+                Le site pramnormandie.com peut contenir des liens hypertextes renvoyant à des pages ou des sites dont le contenu ne peut engager en rien l’association.
+                Les liens hypertextes renvoyant vers notre site sont les bienvenus lorsqu’ils émanent de sites respectant la législation en vigueur.
+                </p><br>
+				<div class="form-check form-switch">
+					<input class="form-check-input" type="checkbox" id="cgu_c">
+					<label class="form-check-label" for="cgu_c"><strong>J'ai lu et j'accepte les conditions générales d'utilisation</strong></label>
+				</div>
+			</div>
+		</form>
+      </div>
+      <div class="modal-footer">
+		<button type="button" id="save_create_account" class="btn btn-primary"  aria-hidden="true">Enregistrer</button>
+      </div>
     </div>
+  </div>
 </div>
+
 
 
 <!-- JQUERY-->
--- php/captcha.php
+++ php/captcha.php
@@ -17,17 +17,17 @@ function generateRandomString($length = 6) {
 
 $_SESSION['n_n'] = generateRandomString();
 //mt_rand(100,9999);
-$img = imagecreate(240,80);
+$img = imagecreate(200,30);
 $font = getcwd().'/../fonts/Multistrokes.ttf';
 $bg = imagecolorallocate($img,255,255,255);
 $textcolor = imagecolorallocate($img,110,193,77);
 
 $pixel_color = imagecolorallocate($img, 110,193,77);
-for($i=0;$i<8000;$i++) {
-    imagesetpixel($img,rand()%240,rand()%80,$pixel_color);
+for($i=0;$i<4000;$i++) {
+    imagesetpixel($img,rand()%200,rand()%80,$pixel_color);
 }  
 
-imagettftext($img, 40,0,12,52, $textcolor, $font,$_SESSION['n_n']);
+imagettftext($img, 30,0,12,32, $textcolor, $font,$_SESSION['n_n']);
 
 header('Content-type:image/jpeg');
 imagejpeg($img);
